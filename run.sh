65c67efd35d481926d5660f8547f792578a2c3f1
swase <gouwsf91@gmail.com>
2024-04-06 20:39:47 +0000

    edit run scripte

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source ./setup.sh #run when needed
+# source ./setup.sh #run when needed
 
 #Run flask app
+pip install --only-binary :all: greenlet
+pip install --only-binary :all: Flask-SQLAlchemy
 flask --app flaskr run --debug
