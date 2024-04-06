dc653aedd91317d9f3a13f476f79f408ae44f5a3
swase <gouwsf91@gmail.com>
2024-04-06 20:50:30 +0000

    run changes

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # source ./setup.sh #run when needed
 
 #Run flask app
-pip install --only-binary :all: greenlet
-pip install --only-binary :all: Flask-SQLAlchemy
-flask --app flaskr run --debug
+pip3.7 -m pip install -r requirements.txt
+# pip install --only-binary :all: greenlet
+# pip install --only-binary :all: Flask-SQLAlchemy
+# flask --app flaskr run --debug
