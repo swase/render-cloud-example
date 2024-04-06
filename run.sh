b0cd9fd31bbe6fee20fd5d42de1e464146cea458
swase <gouwsf91@gmail.com>
2024-04-06 21:13:53 +0000

    changing requirements

--- run.sh
+++ run.sh
@@ -4,6 +4,4 @@
 
 #Run flask app
 python3.7 -m pip install -r requirements.txt
-# pip install --only-binary :all: greenlet
-# pip install --only-binary :all: Flask-SQLAlchemy
 # flask --app flaskr run --debug
